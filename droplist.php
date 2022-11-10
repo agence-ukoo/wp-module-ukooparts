d62c3ce19f4a563fb1be99e616f1a46cef818064
adam.sebbat <adam.sebbat@uha.fr>
2022-11-10 16:04:02 +0100

    droplist affiche tout depuis la bdd

--- droplist.php
+++ droplist.php
@@ -1,63 +1,10 @@
 <!DOCTYPE html>
 <head>
     <meta charset="utf-8" />
-    <link rel="stylesheet"  href = "style.css"/>
+    <link rel="stylesheet" />
 </head>
 <body>
-    
-
-
-
-<section class="dropall">
-<div class = 'droplist'>
-<?php $marques ?>
-<form action="droplist.php">
-  <label for="Marque">Marque</label>
-  <select name="Marque" id="Marque">
-    <option value="">Marque</option>
-    <option value="">Aprilia</option>
-    <option value="">BMW</option>
-    <option value="">Honda</option>
-    <option value="">KTM</option>
-    <option value="">Kawazaki</option>
-    <option value="">Moto-Guizzi</option>
-  </select>
-</form>
-</div>
-
-
-<div class = 'droplist'>
-<form action="droplist.php">
-  <label for="cylindre">Cylindré</label>
-  <select name="cylindre" id="cylindre">
-    <option value="">cylindré</option>
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
-  </select>
-</form>
-</div>
-
-<div class = 'droplist'>
-<form action="droplist.php">
-  <label for="modeles">Modèles</label>
-  <select name="modeles" id="modeles">
-    <option value="">modèles</option>
-    <option value=""></option>
-    <option value=""></option>
-    <option value=""></option>
-  </select>
-</form>
-</div>
-</section>
-
-</body>
-
-
-
-<?php 
-//base de donne en cour
-
+ <?php    
 try{
     $db = new PDO('mysql:host=localhost;dbname=test','root','');
     $db -> exec('SET NAMES "UTF8"');
@@ -65,27 +12,64 @@ try{
     echo 'Erreur:'.$e ->getMessage();
     die();
 }
-
-$reponse = $db->query('SELECT model FROM PREFIX_ukooparts_engine');
- while ($donnees = $reponse->fetch())
- {
-        echo '<select> <option>'.$donnees['model'].'</option> </select>' ;
- }
-
-  $reponse = $db->query('SELECT name FROM PREFIX_ukooparts_manufacturer');
- while ($donnees = $reponse->fetch())
-  {
-       echo '<select> <option>'.$donnees['name'].'</option> </select>' ;
-  }
-
-
- $reponse = $db->query('SELECT * FROM test');
-  while ($donnees = $reponse->fetch())
-  {
-         echo '<select> <option>'.$donnees[''].'</option> </select>' ;
-  }
-
 ?>
+
+<section class="dropall">
+<div class = 'droplist'>
+<?php $marques ?>
+<form action="droplist.php">
+  <label for="Marque">Marque</label>
+  <select>
+<?php
+    foreach ($db->query('SELECT name FROM PREFIX_ukooparts_manufacturer') as $row) {
+        echo '<option value="' . $row['name'] . '">'. $row['name'] . ' </option>';
+    }   
+?>
+</select>
+</form>
+</div>
+
+
+<div class = 'droplist'>
+<form action="droplist.php">
+  <label for="cylindre">Cylindré</label>
+  <select>
+<?php
+    foreach ($db->query('SELECT displacement FROM PREFIX_ukooparts_engine') as $row) {
+        echo '<option value="' . $row['displacement'] . '">'. $row['displacement'] . ' </option>';
+    }   
+?>
+</select>
+</form>
+</div>
+
+<div class = 'droplist'>
+<form action="droplist.php">
+  <label for="model">Modèles</label>
+  <select>
+<?php
+    foreach ($db->query('SELECT model FROM PREFIX_ukooparts_engine') as $row) {
+        echo '<option value="' . $row['model'] . '">'. $row['model'] . ' </option>';
+    }   
+?>
+</select>
+
+<div class = 'droplist'>
+<form action="droplist.php">
+  <label for="year">année</label>
+  <select>
+<?php
+    foreach ($db->query('SELECT  FROM PREFIX_ukooparts_engine') as $row) {
+        echo '<option value="' . $row['year'] . '">'. $row['year'] . ' </option>';
+    }   
+?>
+</select>
+
+</form>
+</div>
+</section>
+
+</body>
 
 
 
