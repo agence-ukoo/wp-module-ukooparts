2920d3861af4b64f87a959c1ef43659278f6452f
Nathan.Engelmann <nathan.engelmann@uha.fr>
2022-11-22 11:58:17 +0100

    Changement de méthode pour call des fichiers js et css. Un peu de clean de ukooparts.php

--- ukooparts/ukooparts.php
+++ ukooparts/ukooparts.php
@@ -17,7 +17,15 @@ require UKOOPARTS_PLUGIN_DIR . 'vendor/autoload.php';
 
 $plugin = new Ukoo\Ukooparts\UkooPartsPlugin(__FILE__);
 
+add_action('wp_head', 'import_script');
 
+function import_script(){
+    ?>
+    <link href="<?php echo plugin_dir_url(__FILE__) ?>css/footer_manufacturers.css" rel="stylesheet">
+    <link href="<?php echo plugin_dir_url(__FILE__) ?>css/footer_types.css" rel="stylesheet">
+    <link href="<?php echo plugin_dir_url(__FILE__) ?>css/top50.css" rel="stylesheet">
+    <?php
+}
 
 // function to create pages  yuhan/////////////////////////////////////////////////////////////////////:
 function create_page($title, $content, $status){
@@ -77,14 +85,13 @@ add_shortcode('manufacturers', 'shortcode_manufacturers');
 
 ////////////////////////////////////ilies////////////////////////////////////////////
 add_action('wp_footer','marque');
-add_action( 'wp_footer','typesCss1');
 
-function marque(){
+function marque(): void{
     printf(' <div class="titrediv" id="titrecss">
             <h2 id="titre"><span> Nos constructeurs moto route </span></h2>
             </div>
-            <div class="triangleRouge" id="triangle">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Red_triangle.svg/540px-Red_triangle.svg.png" />
+            <div class="triangle_container">
+            <img id="triangle" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Red_triangle.svg/540px-Red_triangle.svg.png" />
             </div>
             <div class="container" id="containerLogo">
             <div class="logo" id="yamaha">
@@ -107,76 +114,6 @@ function marque(){
         );
 }
 
-function typesCss1(){
-    echo "
-    <style type='text/css'>
-
-.logo{
-    width: 300px;
-    height: 300px;
-    display: flex;
-    margin-left: auto
-
-}
-#yamaha{
-    border: 1px solid blue;
-   
-
-}
-
-    #titre {
-        position: relative;
-        overflow: hidden;
-        text-align: center;
-        width: 86%;
-        font-weight: bold;
-        font-size: 130%;
-        text-transform: uppercase;
-    }
-    #titrecss{
-        display: flex;
-        width: 100%;
-        justify-content:center;
-    }
-
-    #titre:before,  #titre:after {
-        position: absolute;
-        top: 51%;
-        overflow: hidden;
-        width: 50%;
-        height: 1px;
-        margin-left: 1%;
-        content: '\a0';
-        background-color: 
-    #000;
-    }
-    #titre:before {
-        margin-left: -51%;
-        text-align: right;
-    }
-
-    
-    #containerLogo{
-        margin: 3%;
-        display: flex;
-        flex-direction: row;
-    }
-
-    
-    #triangle{
-        width: 1%;
-        height: 1%;
-        margin-top: 1%;
-        margin-left: 50%;
-        display: flex;
-        flex-direction: row;
-        position: center;
-        align-self: center;
-    }
-    </style>
-    ";
-}
-
 
 /////////////////////////////////////////larbi///////////////////////////////////////////
 
@@ -185,9 +122,10 @@ function types(){
     <div id="containerTitleSelectTypeVehicule">
         <h3 id="titleSelectTypeVehicule"><span>sélectionnez votre type de véhicule</span></h3>
     </div>
-    <div class="redArrow">
-    <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Red_triangle.svg/540px-Red_triangle.svg.png" alt="">
-</div>
+    
+    <div class="triangle_container">
+            <img id="triangle" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Red_triangle.svg/540px-Red_triangle.svg.png" />
+    </div>
 
     <div id="container">
         <div id="containerListTypeVehicule">
@@ -221,88 +159,6 @@ function types(){
     ');
 }
 
-function typesCss(){
-    echo "
-	<style type='text/css'>
-
-    .redArrow{
-        width: 1%;
-        height: 1%;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    #container{
-        display: flex;
-        justify-content: center;
-    }
-
-    #containerTitleSelectTypeVehicule{
-        display: flex;
-        width:100%;
-        justify-content: center;
-    }
-
-
-    #titleSelectTypeVehicule{
-        position: relative;
-        overflow: hidden;
-        text-align: center;
-        font-weight: bold;
-        font-size: 20px;
-        text-transform: uppercase;
-        width: 90%;
-    }
-
-    #titleSelectTypeVehicule:after, #titleSelectTypeVehicule:before {
-        content: '\a0';
-        overflow: hidden;
-        position: absolute;
-        height: 0.8px;
-        width: 50%;
-        top: 51%;
-        margin-left: 1%;
-        background: black;
-    }
-
-    #titleSelectTypeVehicule:before{
-    margin-left: -51%;
-    text-align: right;
-    }
-
-	#containerListTypeVehicule{
-        list-style: none;
-        display: flex;
-        width: 55%;
-        /*justify-content: space-around;*/
-        column-gap: 10%;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    .linkTypeVehicule{
-        outline: none;
-        text-decoration: none;
-    }
-
-    .linkImglistTypeVehicule{
-        display: block;
-        width: 25%;
-    }
-
-    .linkImglistTypeVehicule p {   
-        text-align: center;
-        white-space: nowrap;
-
-    }
-
-   
-
-	</style>
-	";
-}
-
-add_action('wp_footer', 'typesCss');
 add_action('wp_footer', 'types');
 
 function shortcode_cadeaux(): string{
@@ -373,7 +229,7 @@ function shortcode_models() {
                 $manufact_id = $_GET['manufact_id'];
                 $engine_type_id = $_GET['engine_type_id'];
 
-                $models = (cal_from_jd()->query("SELECT distinct engine.model, manu.name, type.name AS type_name
+                $models = (call_bdd()->query("SELECT distinct engine.model, manu.name, type.name AS type_name
                     FROM PREFIX_ukooparts_engine AS engine
                     INNER JOIN PREFIX_ukooparts_manufacturer AS manu
                     ON manu.id_ukooparts_manufacturer = engine.id_ukooparts_manufacturer
@@ -421,9 +277,7 @@ add_shortcode('models', 'shortcode_models');
 // Larbi top50 moto
 function shortcode_topmoto(): string{
     try{
-        $db = new PDO('mysql:host=localhost;dbname=ukooparts','root','');
-        $db -> exec('SET NAMES "UTF8"');
-        $motoData = $db->query("SELECT * FROM PREFIX_ukooparts_engine_lang LIMIT 50")->fetchAll(PDO::FETCH_ASSOC);
+        $motoData = call_bdd()->query("SELECT * FROM PREFIX_ukooparts_engine_lang LIMIT 50")->fetchAll(PDO::FETCH_ASSOC);
 
         $string = "";
         $string .= "<ol id='order_list_vehicle'>";
@@ -445,21 +299,3 @@ add_shortcode('topmoto', 'shortcode_topmoto');
 
 /*************      Test CSS  ********/
 
-add_action('wp_enqueue_scripts', 'call_top50_assets');
-
-function call_top50_assets():void{
-    load_assets('css/ukooparts.css');
-}
-
-function load_assets($path): void
-{
-
-    wp_enqueue_style(
-        'ukooparts',
-        plugin_dir_url(__FILE__) . $path,
-        array(),
-        1,
-        'all'
-    );
-
-}
